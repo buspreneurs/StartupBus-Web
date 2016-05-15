c0e94caf23b3d7515bde26da36c495638507c954
Carl Shotwell <shotwellcarl@gmail.com>
2016-05-15 09:12:52 -0400

    Fix floating caption height

--- js/bustracker.js
+++ js/bustracker.js
@@ -4,73 +4,71 @@ function initialize() {
     var myLatlng = new google.maps.LatLng(39.4053947,-95.363801);
     var mapOptions = {
         zoom: 4,
-        center: myLatlng
+        center: myLatlng,
+        scrollwheel: false,
+        draggable: false,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
     }
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    window.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    var starting_cities = {
+      cities : [
+      { city: "San Francisco",
+        origin: [37.7682851,-122.4205412],
+        marker: "images/markers/SF.png",
+      },
+      { city: "Akron",
+        origin: [41.0771553,-81.5137661],
+        marker: "images/markers/AKRON.png",
+      },
+      { city: "New York City",
+        origin: [40.7277495,-73.9924231],
+        marker: "images/markers/NYC.png",
+      },
+      { city: "Mexico City",
+        origin: [19.4340894,-99.1323558],
+        marker: "images/markers/MEXICO.png",
+      },
+      { city: "Tampa",
+        origin: [27.9420771,-82.4707102],
+        marker: "images/markers/TAMPA.png",
+      },
+      { city: "Vancouver",
+        origin: [49.2562176,-123.1939532],
+        marker: "images/markers/VAN.png",
+       },
+      { city: "Boulder",
+        origin: [40.0294203,-105.3100175],
+        marker: "images/markers/FINALS.png",}
+      ]
+    };
 
     var cities = []
     addBusMarkers();
 
     var points = []
-
-    function fetch_bus_icon(bus) {
-        var marker;
-        switch(bus) {
-            case "San Francisco":
-                marker = "static/images/Carl_pin_OJ_3.png";
-                break;
-            case "Vancouver":
-                marker = "static/images/Carl_pin_blue_3.png";
-                break;
-            case "Mexico City":
-                marker = "static/images/Carl_pin_red_3.png";
-                break;
-            case "Akron":
-                marker = "static/images/Carl_pin_pink_3.png";
-                break;
-            case "New York City":
-                marker = "static/images/Carl_pin_purp_3.png";
-                break;
-            case "Tampa":
-                marker = "static/images/Carl_pin_green_3.png";
-                break;
-            default:
-                marker = "static/images/Carl_pin_ugh_3.png";
-                break;
-        }
-        return marker;
-    }
-
     function fetch_bus_line_color(bus) {
-        var marker;
-        switch(bus) {
-            case "Tampa":
-                marker = "#FEB000";
-                break;
-            case "Mexico City":
-                marker = "#22DCFF";
-                break;
-            case "New York City":
-                marker = "#FC4E00";
-                break;
-            case "Akron":
-                marker = "#F012D6";
-                break;
-            case "Southeast":
-                marker = "#C901F3";
-                break;
-            case "Vancouver":
-                marker = "#32E972";
-                break;
-            default:
-                marker = "static/images/Carl_pin_ugh_3.png";
-                break;
+        var line;
+        switch(bus) { 
+        case "San Francisco":
+          line: "#6bc8d1";
+        case "Akron":
+          line: "#fc0f3f";
+        case "New York City":
+          line: "#fee834";
+        case "Mexico City":
+          line: "#ee9532";
+        case "Tampa":
+          line: "#246bd1";
+        case "Vancouver":
+          line: "#ab3f90";
         }
-        return marker;
+
+        return line;
     }
+
 
     $(document).ready(function(){ 
-
         $.ajax({
           url: "http://subtracker.herokuapp.com/allPointsLowPrecision/2016",
             success: function(data){
@@ -88,51 +86,37 @@ function initialize() {
                         strokeOpacity: 1.0,
                         strokeWeight: 5
                     });
-                    flightPath.setMap(map);
+                    flightPath.setMap(window.map);
                 });
             }, //success
         jsonpCallback: 'jsonCallback',
         contentType: "application/json",
         dataType: 'jsonp'
         });
+
+        var current_bounds = window.map.getBounds();
+        var van_pos = new google.maps.LatLng(49.2562176,-123.1939532);
+        var tampa_pos = new google.maps.LatLng(27.9420771,-82.4707102);
+
+        if( !current_bounds.contains(van_pos) || !current_bounds.contains(tampa_pos)){
+          var bounds = new google.maps.LatLngBounds();
+          bounds.extend(van_pos);
+          bounds.extend(tampa_pos);
+          window.map.fitBounds(bounds);
+        }
     }); //document.ready
 
-      var starting_cities = {
-        cities : [
-        { city: "San Francisco",
-          origin: [37.7682851,-122.4205412],
-          marker: "images/markers/SF.png",
-          line_color: "6bc8d1"},
-        { city: "Akron",
-          origin: [41.0771553,-81.5137661],
-          marker: "images/markers/AKRON.png",
-          line_color: ""},
-        { city: "New York City",
-          origin: [40.7277495,-73.9924231],
-          marker: "images/markers/NYC.png",
-          line_color: ""},
-        { city: "Mexico City",
-          origin: [19.4340894,-99.1323558],
-          marker: "images/markers/MEXICO.png",
-          line_color: ""},
-        { city: "Tampa",
-          origin: [27.9420771,-82.4707102],
-          marker: "images/markers/TAMPA.png",
-          line_color: ""},
-        { city: "Vancouver",
-          origin: [49.2562176,-123.1939532],
-          marker: "images/markers/VAN.png",
-          line_color: ""}
-        ]
-      };
-
     function addBusMarkers() {
-
         cities = _.map(starting_cities.cities, function(city) {
+          var icon = {
+            url: city.marker,
+               anchor: new google.maps.Point(17, 34),
+          };
+
             var new_city = new google.maps.Marker({
                 position: new google.maps.LatLng(city.origin[0],city.origin[1]), 
                 map: map, 
-                icon: city.marker
+                icon: icon
             });
         });
     }
