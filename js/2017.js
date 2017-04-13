fa42b9cdbda3f3e7e2b404658b358b5e30b57529
William Yaworsky <will.yaworsky@gmail.com>
2017-04-12 21:40:56 -0400

    2017!!

--- js/2017.js
+++ js/2017.js
@@ -1,3 +1,16 @@
-$(window).load(function(){
+$(window).load(function() {
   $("#the-pudding").sticky({ topSpacing: 50, getWidthFrom: '#nav' });
+});
+
+var highlighers = $('.highligher');
+$(window).scroll(function() {
+  var st = $(this).scrollTop();
+  highlighers.each(function(_, h) {
+    var highlighter = $(h);
+    var itsFor      = $(highlighter.data('for'));
+    var top         = itsFor.position().top;
+    if (st > top) {
+      highlighter.addClass('lit');
+    }
+  });
 });
